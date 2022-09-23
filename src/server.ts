622d586b1344314b766e699d1b770edb748aa626
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-23 16:17:49 +0200

    dev: api.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-const express = require("express");
-const serveIndex = require("serve-index");
+import express from "express";
+import serveIndex from "serve-index";
+import { api } from "./api";
 
 const app = express();
 const port = 3000;
 
 const www = "./public";
 
-app.get("/api/date", (req, res) => {
-  res.json({ now: new Date() });
-});
+app.use("/api", api);
 
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
